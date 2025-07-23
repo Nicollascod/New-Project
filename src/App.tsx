d3f6f6e1e4d6792a8bd31c624076eef1abbf4c5b
Nicollascod <nicollascod@gmail.com>
2025-07-23 11:40:39 -0300

    Versão modificada

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-
 import './App.css'
+import { somar, dividir } from './utills/math';
+
 
 function App() {
-  
+  const nome = 'Nicollas';
 
   return (
     <>
-      
+     <div>
+      <h1>Soma: {somar(4, 2)}</h1>
+      <h1>Divisão: {dividir(10, 2)}</h1>
+      ^
+    </div>
     </>
+    
   )
 }
 
